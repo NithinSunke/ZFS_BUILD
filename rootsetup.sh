c0ef7ea16a8810eff5ab2aa81cdf2876cb2d1f6b
Sunke Nithin <sunkentihin@gmail.com>
2024-01-20 03:29:02 +0530

    DA

--- rootsetup.sh
+++ rootsetup.sh
@@ -1,6 +1,5 @@
-. /tmp/ZFS_BUILD/install_env.sh
 sudo sh /tmp/ZFS_BUILD/centos7_os_build.sh
-
+. /tmp/ZFS_BUILD/install_env.sh
 sudo sh /tmp/ZFS_BUILD/zfs_server_install.sh
 
 sudo sh /tmp/ZFS_BUILD/create_zfs_storage_pool.sh
