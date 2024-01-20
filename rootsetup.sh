f536278f2e7a7f7b7ddc5edae9b446d6cd4ca890
Sunke Nithin <sunkentihin@gmail.com>
2024-01-20 13:58:43 +0530

    d

--- rootsetup.sh
+++ rootsetup.sh
@@ -4,9 +4,9 @@ sudo sh /tmp/ZFS_BUILD/zfs_server_install.sh
 
 sudo sh /tmp/ZFS_BUILD/create_zfs_storage_pool.sh
 
-sudo sh /tmp/ZFS_BUILD/creat_zfs_dataset.sh
+sudo sh /tmp/ZFS_BUILD/create_zfs_dataset.sh
 
-sudo sh /tmp/ZFS_BUILD/creat_zfs_volume.sh
+sudo sh /tmp/ZFS_BUILD/create_zfs_volume.sh
 
 sudo sh /tmp/ZFS_BUILD/install_iscsi_server.sh
 
